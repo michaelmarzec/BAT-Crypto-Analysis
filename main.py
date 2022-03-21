386bd29d8afd8e25d4612041083f82b683213949
Michael Marzec <michaelmarzec11@gmail.com>
2022-03-21 17:19:38 -0600

    main_update

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+#### structure / guide ####
+# imports
+# functions
+## fetch_daily_data --> api call for all data (good for automated pipeline)
+## extract_data --> extraneous
+## read_data --> read pre-saved parquet (good for ad-hoc usage)
+## created_merged_df --> 
+## create_plot --> 
+## correlation_plot --> 
+## table_plot --> 
+## rolling_correlation --> 
+## create_holdings_portfolio --> 
+## bat_acquisition --> 
+## calc_hold_only_roi --> 
+## roi_plot --> 
+## series_to_supervised --> ##### predict the price of BAT (relative to USD) # https://machinelearningmastery.com/xgboost-for-time-series-forecasting/
+## train_test_split -->
+## walk_forward_validation --> ##### walk-forward validation for univariate data (it's expanding window)
+## xgboost_forecast --> ####fit an xgboost model and make a one step prediction
+## xgBoost_model --> 
+## trading_portfolio --> 
+
+# main
+## parameters
+## processing
+    ### ad-hoc
+    ### pipeline/data update
+## graphs
+
+
 # First import the libraries that we need to use
 import datetime
 import json
@@ -75,11 +105,9 @@ def fetch_daily_data(symbol, start, end, api_call_limit=300):
 
     return full_df
 
-
 def extract_data(token_pair, start, end):
     dt = fetch_daily_data(symbol=token_pair, start=start, end=end)
     return dt
-
 
 def read_data(file_name, cols=['date','close','volume']):
     dt = pd.read_parquet('data/' + file_name)
@@ -217,197 +245,100 @@ def roi_plot(df, plt_show=False, plt_save=False, png_name='roi_plot.png'):
     if plt_save == True:
         plt.savefig(png_name)
 
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[0]
+    df = pd.DataFrame(data)
+    cols = list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg.values
 
-    
-if __name__ == "__main__":
-    print('execution started')
-    # extract data
-    # BTC_USD = extract_data('BTC/USD', '2017-11-09', '2022-02-28')
+def train_test_split(data, n_test):
+    output = data[:-n_test, :], data[-n_test:, :]
+    return output
 
-    # read data
-    BAT_BTC = read_data('BAT-BTC.parquet')
-    BAT_USD = read_data('BAT-USD.parquet')
-    BTC_USD = read_data('BTC-USD.parquet')
+def walk_forward_validation(data, n_test):
+    predictions = list()
+    # split dataset
+    train, test = train_test_split(data, n_test)
+    # seed history with training dataset
+    history = [x for x in train]
+    # step over each time-step in the test set
+    for i in range(len(test)):
+        # split test row into input and output columns
+        testX, testy = test[i, :-1], test[i, -1]
+        # fit model on history and make a prediction
+        yhat = xgboost_forecast(history, testX)
+        # store forecast in list of predictions
+        predictions.append(yhat)
+        # add actual observation to history for the next loop
+        history.append(test[i])
+    # estimate prediction error
+    error = mean_absolute_error(test[:, -1], predictions)
+    return error, test[:, 1], predictions
 
-    # # create close dataframe
-    # close_df = create_merged_df(BAT_BTC, BAT_USD, BTC_USD, 'close')
+def xgboost_forecast(train, testX):
+    # transform list into array
+    train = np.asarray(train)
+    # split into input and output columns
+    trainX, trainy = train[:, :-1], train[:, -1]
+    # fit model
+    model = XGBRegressor(objective='reg:squaredlogerror', n_estimators=1000) #try: squaredlogerror; squarederror
+    model.fit(trainX, trainy)
+    # make a one-step prediction
+    yhat = model.predict(np.asarray([testX]))
+    return yhat[0]
 
-    # # print graph
-    # create_plot(close_df, ['BAT_USD'], False, False, 'plots/BAT_USD_Price_Plot.png')
-    # create_plot(close_df, ['BTC_USD'], False, False, 'plots/BTC_USD_Price_Plot.png')
-
-    # # correlation test + table
-    # close_corr_table = close_df.corr()
-    # table_plot(close_corr_table, False, False, 'plots/correlation_table.png')
-
-    # # rolling correlation
-    # rolling_correlation_df = rolling_correlation(close_df)
-    # correlation_plot(rolling_correlation_df, False, False, 'plots/rolling_correlation.png')
-
-
-    # ### market holdings
-
-    start_date = '2021-01-01'
-    # # today = datetime.datetime.now().strftime("%Y-%m-%d")
-    today = '2022-02-28'
-
-    # hold_only_portfolio, trade_dates = create_holdings_portfolio(start_date, today, hold_only_columns = ['BAT','USD','BTC'])
-    # hold_only_portfolio = bat_acquisition(hold_only_portfolio, today, trade_dates, BAT_USD, BAT_BTC)
-
-
-    # ### ROI conversion/analysis (convert everything to USD for appropriate $ analysis)
-    # hold_only_portfolio_roi = calc_hold_only_roi(hold_only_portfolio, BAT_USD, BTC_USD, start_date, today)
-    # roi_plot(hold_only_portfolio_roi, False, False, 'plots/roi_plot.png')
-
-
-
-    # predict the price of BAT (relative to USD) # https://machinelearningmastery.com/xgboost-for-time-series-forecasting/
-    def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
-        n_vars = 1 if type(data) is list else data.shape[0]
-        df = pd.DataFrame(data)
-        cols = list()
-        # input sequence (t-n, ... t-1)
-        for i in range(n_in, 0, -1):
-            cols.append(df.shift(i))
-        # forecast sequence (t, t+1, ... t+n)
-        for i in range(0, n_out):
-            cols.append(df.shift(-i))
-        # put it all together
-        agg = pd.concat(cols, axis=1)
-        # drop rows with NaN values
-        if dropnan:
-            agg.dropna(inplace=True)
-        return agg.values
-
-    def train_test_split(data, n_test):
-        return data[:-n_test, :], data[-n_test:, :]
-
-
-    # walk-forward validation for univariate data
-    def walk_forward_validation(data, n_test):
-        predictions = list()
-        # split dataset
-        train, test = train_test_split(data, n_test)
-        # seed history with training dataset
-        history = [x for x in train]
-        # step over each time-step in the test set
-        for i in range(len(test)):
-            # split test row into input and output columns
-            testX, testy = test[i, :-1], test[i, -1]
-            # fit model on history and make a prediction
-            yhat = xgboost_forecast(history, testX)
-            # store forecast in list of predictions
-            predictions.append(yhat)
-            # add actual observation to history for the next loop
-            history.append(test[i])
-        # estimate prediction error
-        error = mean_absolute_error(test[:, -1], predictions)
-        return error, test[:, 1], predictions
-
-    # fit an xgboost model and make a one step prediction
-    def xgboost_forecast(train, testX):
-        # transform list into array
-        train = np.asarray(train)
-        # split into input and output columns
-        trainX, trainy = train[:, :-1], train[:, -1]
-        # fit model
-        model = XGBRegressor(objective='reg:squaredlogerror', n_estimators=1000) #try: squaredlogerror; squarederror
-        model.fit(trainX, trainy)
-        # make a one-step prediction
-        yhat = model.predict(np.asarray([testX]))
-        return yhat[0]
-
-    
-
-
-    #CV: Iterable parameters
-        # objective: reg:squarederror reg:squaredlogerror
-        # n_estimators: default = 1000
-        # n_in (how far to look back)
-        # n_test (# of days to estiamte)
-        # eval metrics: MAE
-    # from matplotlib import pyplot
-
-    
-    # for i in range(1,31):
-    # data = series_to_supervised(BAT_USD['close'],n_in=30)
-    # mae, y, yhat = walk_forward_validation(data, 31)
-    # print(mae)
-    # print(str(i) + ' - ' + str(mae))
-        # print(y)
-        # print(yhat)
-
-    # print(y)
-    # print(yhat)
-    # pyplot.plot(y, label='Expected')
-    # pyplot.plot(yhat, label='Predicted')
-    # pyplot.legend()
-    # pyplot.show()
-
-    # # transform the time series data into supervised learning
-    # train = series_to_supervised(BAT_USD['close'], n_in=30)
-    # # split into input and output columns
-    # trainX, trainy = train[:, :-1], train[:, -1]
-
-    # # fit model
-    # model = XGBRegressor(objective='reg:squaredlogerror', n_estimators=1000)
-    # model.fit(trainX, trainy)
-
-    # # construct an input for a new preduction
-    # row = BAT_USD['close'].values[-30:]
-
-    # # make a one-step prediction
-    # yhat = model.predict(np.asarray([row]))
-    # print('Input: %s, Predicted: %.3f' % (row, yhat[0]))
-
-
-    trading_df = BAT_USD.set_index('date')
+def xgBoost_model(bat_usd, trade_start_dt, today_dt, training_window=60, xg_objective='reg:squaredlogerror', xg_n_estim=1000):
+    trading_df = bat_usd.set_index('date')
     trading_df = trading_df['close']
 
-    trade_start_date = '2021-02-01'
-
-####### uncomment #####################################
-
-    trade_start_date = datetime.datetime.strptime(trade_start_date, '%Y-%m-%d')
-    trade_end_date = datetime.datetime.strptime('2022-02-28', '%Y-%m-%d')
-    # delta = trade_end_date - trade_start_date
+    trade_start_dt = datetime.datetime.strptime(trade_start_dt, '%Y-%m-%d')
+    trade_end_date = datetime.datetime.strptime(today_dt, '%Y-%m-%d')
+    delta = trade_end_date - trade_start_dt
 
 
-    # price_predictions = []
-    # for i in range(delta.days + 1):
-    #     train_start = trade_start_date - datetime.timedelta(days=59) - pd.DateOffset(days=1) + datetime.timedelta(days=i)
-    #     train_end = trade_start_date - pd.DateOffset(days=1) + datetime.timedelta(days=i)
-    #     train_data = trading_df[train_start:train_end]
+    price_predictions = []
+    for i in range(delta.days + 1):
+        train_start = trade_start_dt - datetime.timedelta(days=(training_window-1)) - pd.DateOffset(days=1) + datetime.timedelta(days=i)
+        train_end = trade_start_dt - pd.DateOffset(days=1) + datetime.timedelta(days=i)
+        train_data = trading_df[train_start:train_end]
 
-    #     train = series_to_supervised(train_data, n_in=30)
-    #     trainX, trainy = train[:, :-1], train[:, -1]
+        train = series_to_supervised(train_data, n_in=30)
+        trainX, trainy = train[:, :-1], train[:, -1]
 
-    #     model = XGBRegressor(objective='reg:squaredlogerror', n_estimators=1000)
-    #     model.fit(trainX, trainy)
+        model = XGBRegressor(objective=xg_objective, n_estimators=xg_n_estim)
+        model.fit(trainX, trainy)
 
-    #     row = train_data.values[-30:]
-    #     yhat = model.predict(np.asarray([row]))
-    #     price_predictions.append(yhat[0])
+        row = train_data.values[-30:]
+        yhat = model.predict(np.asarray([row]))
+        price_predictions.append(yhat[0])
     
-    # trading_df = trading_df[trade_start_date:trade_end_date]
-    # trading_df = pd.DataFrame(trading_df)
-    # trading_df['predictions'] = price_predictions
-########################################################
+    trading_df = trading_df[trade_start_dt:trade_end_date]
+    trading_df = pd.DataFrame(trading_df)
+    trading_df['predictions'] = price_predictions
 
+    mae = mean_absolute_error(trading_df['close'].values, trading_df['predictions'].values)
 
-    trading_df = pd.read_csv('test.csv')
+    return trading_df, mae
 
-    trading_df['date'] = pd.to_datetime(trading_df['date'])
-    trading_df.set_index('date',inplace=True)
+def trading_portfolio(trading_df, trade_start_dt, start_date, today):
+    trade_start_dt = datetime.datetime.strptime(trade_start_dt, '%Y-%m-%d')
 
-    error = mean_absolute_error(trading_df['close'].values, trading_df['predictions'].values)
-    
     trading_df['cash_holdings'] = np.nan
     trading_df['bat_holdings'] = np.nan
     
-
-    trading_df.loc[trade_start_date,['cash_holdings']] = 0
-    trading_df.loc[trade_start_date,['bat_holdings']] = 200
+    trading_df.loc[trade_start_dt,['cash_holdings']] = 0
+    trading_df.loc[trade_start_dt,['bat_holdings']] = 200
 
     trading_df['bat_trade'] = np.where(trading_df['predictions'].shift(-1) > trading_df['close'],'buy','sell')
 
@@ -417,7 +348,7 @@ if __name__ == "__main__":
     trading_df['bat_holdings'] = trading_df['bat_holdings'].ffill()
 
     for index, row in trading_df.iterrows():
-        if index == trade_start_date:
+        if index == trade_start_dt:
             cur_action = row['bat_trade']
             cur_close = row['close']
             cur_cash = row['cash_holdings']
@@ -444,8 +375,104 @@ if __name__ == "__main__":
             cur_cash = trading_df.loc[index]['cash_holdings']
             cur_bat = trading_df.loc[index]['bat_holdings']
 
+    return trading_df
+
+def final_roi_df(hold_df, trade_df, bat_usd):
+    df_roi = hold_df.copy()
+
+    start_date = df_roi.iloc[0].name
+    end_date = df_roi.iloc[-1].name
+
+    BAT_USD_roi = bat_usd.set_index('date')
+    BAT_USD_roi = BAT_USD_roi.loc[start_date:end_date]['close']
+
+    df_roi['TRADE_ROI'] = trade_df['bat_holdings'] * BAT_USD_roi + trade_df['cash_holdings']
+    df_roi['TRADE_ROI'] = df_roi['TRADE_ROI'].fillna(0)
+
+    return df_roi
+
+def final_roi_plot(df, plt_show=False, plt_save=False, png_name='roi_plot.png'):
+    fig, ax = plt.subplots()
+    plt.plot(df)
+    plt.title('ROI/Price Plot')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    fmt = '${x:,.2f}'
+    tick = mtick.StrMethodFormatter(fmt)
+    ax.yaxis.set_major_formatter(tick) 
+    plt.legend(['USD_ROI','BAT_ROI','BTC_ROI','TRADE_ROI'])
+    if plt_show == True:
+        plt.show()
+    if plt_save == True:
+        plt.savefig(png_name)
+
+
+
+if __name__ == "__main__":
+    print('execution started')
+    ## parameters ##
+    start_date = '2021-01-01'
+    trade_start_date = '2021-02-01'
+    today = '2022-02-28'
+
+    ## processing ##
+    ### ad-hoc ###
+
+    # read data
+    BAT_BTC = read_data('BAT-BTC.parquet')
+    BAT_USD = read_data('BAT-USD.parquet')
+    BTC_USD = read_data('BTC-USD.parquet')
+
+    # create close dataframe
+    close_df = create_merged_df(BAT_BTC, BAT_USD, BTC_USD, 'close')
+
+    # correlation test + table
+    close_corr_table = close_df.corr()
+    rolling_correlation_df = rolling_correlation(close_df)
     
+    # market holdings
+    hold_only_portfolio, trade_dates = create_holdings_portfolio(start_date, today, hold_only_columns = ['BAT','USD','BTC'])
+    hold_only_portfolio = bat_acquisition(hold_only_portfolio, today, trade_dates, BAT_USD, BAT_BTC)
+
+    # ROI conversion/analysis (convert everything to USD for appropriate $ analysis)
+    hold_only_portfolio_roi = calc_hold_only_roi(hold_only_portfolio, BAT_USD, BTC_USD, start_date, today)
+
+    #### use for full processing results ####
+    # # create xg_boost predictions
+    # trading_df, mae = xgBoost_model(BAT_USD, trade_start_date, today)
+    # trading_df.to_csv('xgBoost_results.csv')
+    ########
+
+    #### use if local 'test.csv' contains xgBoost_model trading_df ####
+    trading_df = pd.read_csv('xgBoost_results.csv')
+    trading_df['date'] = pd.to_datetime(trading_df['date'])
+    trading_df.set_index('date',inplace=True)
+    ########
+
+    trading_df = trading_portfolio(trading_df, trade_start_date, start_date, today)
+
+
+    final_roi = final_roi_df(hold_only_portfolio_roi, trading_df, BAT_USD)
     
+
+    ### automated pipeline / data update ###
+    
+    # # extract data
+    # #BTC_USD = extract_data('BTC/USD', '2017-11-09', '2022-02-28')
+    # today = datetime.datetime.now().strftime("%Y-%m-%d") #automated pipeline
+
+    ## graphs
+
+    # # print graph
+    # create_plot(close_df, ['BAT_USD'], False, False, 'plots/BAT_USD_Price_Plot.png')
+    # create_plot(close_df, ['BTC_USD'], False, False, 'plots/BTC_USD_Price_Plot.png')
+
+    # table_plot(close_corr_table, False, False, 'plots/correlation_table.png')
+
+    # correlation_plot(rolling_correlation_df, False, False, 'plots/rolling_correlation.png')
+
+    # roi_plot(hold_only_portfolio_roi, False, False, 'plots/roi_plot.png')
+    # final_roi_plot(final_roi, True, True, 'plots/final_roi.png')
 
 
 
